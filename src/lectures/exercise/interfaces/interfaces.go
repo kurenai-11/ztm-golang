9b2cbdfab502805ee4f8e624f39a3ea6d9356188
kurenai-11 <moldrenkailich@gmail.com>
2023-02-23 11:43:02 +0300

    idk what the **** was I supposed to do in this exercise

--- src/lectures/exercise/interfaces/interfaces.go
+++ src/lectures/exercise/interfaces/interfaces.go
@@ -19,6 +19,36 @@
 
 package main
 
-func main() {
+import "fmt"
 
+type Lifter interface {
+	Lift()
+}
+
+type Vehicle struct {
+	vehicleType string
+	modelName   string
+}
+
+func (v Vehicle) Lift() {
+	fmt.Println("Lifted:", v.modelName)
+	switch v.vehicleType {
+	case "Truck":
+		fmt.Printf("Lifted the truck %v with the large lift\n", v.modelName)
+	case "Car":
+		fmt.Printf("Lifted the car %v with the standard lift\n", v.modelName)
+	case "Motorcycle":
+		fmt.Printf("Lifted the motorcycle %v with the small lift\n", v.modelName)
+	}
+}
+
+func main() {
+	vehicles := []Vehicle{
+		{"Truck", "Grand 02 XL"},
+		{"Car", "Phewer XS"},
+		{"Motorcycle", "Raidle XR"},
+	}
+	for _, v := range vehicles {
+		v.Lift()
+	}
 }

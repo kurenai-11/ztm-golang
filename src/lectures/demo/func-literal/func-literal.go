2f94d0d03985e9f2dd92c8e472f01efc28ae1b1f
kurenai-11 <moldrenkailich@gmail.com>
2023-02-25 13:44:14 +0300

    tried closures

--- src/lectures/demo/func-literal/func-literal.go
+++ src/lectures/demo/func-literal/func-literal.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main() {
-
+	createIterator := func() func() int {
+		counter := 0
+		return func() int {
+			counter++
+			return counter
+		}
+	}
+	iterator := createIterator()
+	fmt.Println(iterator())
+	fmt.Println(iterator())
+	fmt.Println(iterator())
+	fmt.Println(iterator())
+	fmt.Println(iterator())
+	newIterator := createIterator()
+	fmt.Println(newIterator())
+	fmt.Println(newIterator())
+	fmt.Println(newIterator())
+	fmt.Println(newIterator())
+	fmt.Println(newIterator())
+	fmt.Println(iterator())
+	fmt.Println(iterator())
+	fmt.Println(iterator())
+	fmt.Println(newIterator())
+	fmt.Println(newIterator())
+	fmt.Println(newIterator())
 }
